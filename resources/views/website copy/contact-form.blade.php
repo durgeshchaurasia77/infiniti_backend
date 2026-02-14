dac1910b9754b569116be26f82cb6f9a2a6c8c95
Durga <durgesh@infinititechsolution.com>
2026-02-14 20:04:48 +0530

    test

---
+++ resources/views/website copy/contact-form.blade.php
@@ -0,0 +1,78 @@
+        <section class="consult-section">
+            <div class="consult-wrapper">
+
+                <!-- LEFT SIDE -->
+              <div class="left-consult">
+                    <p class="tag">
+                        Partner with tech catalysts who transform ideas into impact.
+                    </p>
+
+                    <p class="sub-tag">Book your free consultation with us.</p>
+                    @php
+                        $settingDataContact    = App\Models\Setting::select('id', 'multiple_address')->first();
+                    @endphp
+                    @if(!empty($settingDataContact->multiple_address))
+                    <h1>Let’s Talk!</h1>
+
+                    <div class="country-slider">
+                        <div class="country-track">
+                        @foreach ($contactdata->multiple_address as $key11=> $office1)
+                            <div class="country @if($key11 == 0) active @endif">
+                                <h3>{{ strtoupper($office1['name']) }}</h3>
+                                <p>{!! nl2br(e($office1['address'])) !!}</p>
+                                {{-- <p>DWTC, Sheikh Zayed Road, Dubai</p>
+                                <p>+971 50 782 1690</p> --}}
+                            </div>
+
+                            {{-- <div class="country">
+                                <h3>UNITED STATES</h3>
+                                <p>42 Broadway, New York, NY 10004</p>
+                                <p>+1 (512) 872 3364</p>
+                            </div>
+
+                            <div class="country">
+                                <h3>UNITED KINGDOM</h3>
+                                <p>Covent Garden, London WC2H 9JQ</p>
+                                <p>+44 20 7183 9424</p>
+                            </div> --}}
+                        @endforeach
+
+                        </div>
+
+                        <div class="nav">
+                            <button class="prev consult-prev-btn">←</button>
+                            <button class="next consult-next-btn">→</button>
+                        </div>
+                    </div>
+                    @endif
+                </div>
+
+                <!-- RIGHT SIDE -->
+                <div class="right-consult">
+                    <div class="form-box">
+                        <h2>Speak With Our Experts</h2>
+
+                        <form action="{{ route('contact-us-form') }}" method="post"  class="formSubmit1" enctype="multipart/form-data">
+                            @csrf
+                            <input type="text" id="name" name="name" placeholder="Your Name" required>
+                            <input type="phone" name="contact" id="phone" placeholder="Phone" required>
+                            <input type="email" id="email" name="email" placeholder="Your Email" required>
+
+                            <select name="launch" required>
+                                <option value="">When do you want to launch a solution?</option>
+                                <option value="Immediately">Immediately</option>
+                                <option value="1–3 Months">1–3 Months</option>
+                                <option value="3–6 Months">3–6 Months</option>
+                            </select>
+
+                            <textarea name="subject" placeholder="About Project"></textarea>
+                            <button class="btn btn-primary  loderButton" style="justify-content: center;">
+                                <span class="spinner-grow spinner-grow-sm loderIcon" role="status" aria-hidden="true" style="display: none;"></span>Send Message
+                            </button>
+                            {{-- <button type="submit">Submit</button> --}}
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </section>
---
+++ resources/views/website copy/layout/footer.blade.php
@@ -0,0 +1,93 @@
+    @php
+        $settingData    = App\Models\Setting::first();
+        $serviceFooterList    = App\Models\Service::select('id','name','seo_slug')->where('status',1)->get();
+        $industryFooterList    = App\Models\Industry::select('id','title','seo_slug')->where('status',1)->get();
+    @endphp
+<footer class="apptunix-footer">
+  <div class="footer-container">
+
+    <!-- BRAND -->
+    <div class="footer-col brand">
+      {{-- <h2 class="logo">Apptunix</h2> --}}
+      <div class="logo header-logo">
+        <a href="{{ route('web_home') }}">
+            <img src="{{ asset($settingData->footer_logo ?? 'website1/assets/images/infiniti-logo.png') }}" alt=""></div>
+      </a>
+      <p>{{ $settingData->footer_about ?? '' }}</p>
+
+      <a href="#" class="footer-btn">Contact Now!</a>
+
+      <div class="sales">
+        <strong>For sales enquiries:</strong>
+        <a href="mailto:{{ $settingData->email ?? 'example@email.com' }}">{{ $settingData->email ?? 'example@email.com' }}</a>
+      </div>
+
+      {{-- <img src="assets/images/dmca.png" class="dmca" alt="DMCA"> --}}
+    </div>
+
+    <!-- RECENT GUIDES -->
+    {{-- <div class="footer-col">
+      <h4>Recent e-guides</h4>
+      <ul>
+        <li><a href="#">App Monetization Strategies: How to Make Money From an App?</a></li>
+        <li><a href="#">A Comprehensive Guide To Mobile App Development in 2025 – 2026</a></li>
+        <li><a href="#">Telemedicine 2.0 – A Comprehensive Guide</a></li>
+        <li><a href="#">Electric Vehicle Software Development – A Comprehensive Guide</a></li>
+      </ul>
+    </div> --}}
+
+    <!-- SERVICES -->
+    <div class="footer-col">
+        @if(count($serviceFooterList) > 0)
+            <h4>Services</h4>
+            <ul>
+            @foreach ($serviceFooterList as $serviceFooter)
+                <li><a href="{{ $serviceFooter->seo_slug }}">{{ $serviceFooter->name ?? '' }}</a></li>
+            @endforeach
+            </ul>
+        @endif
+    </div>
+
+    <!-- EXPERTISE -->
+    <div class="footer-col">
+        @if(count($industryFooterList) > 0)
+            <h4>Industries</h4>
+            <ul>
+            @foreach ($industryFooterList as $industryFooter)
+                <li><a href="{{ $industryFooter->seo_slug }}">{{ $industryFooter->title ?? '' }}</a></li>
+            @endforeach
+            </ul>
+        @endif
+    </div>
+    <div class="footer-col">
+
+            <h4>We Are In Dubai</h4>
+            <ul>
+            <li><a href="{{ route('digital-marketing-dubai') }}">Digital Marketing</a></li>
+            <li><a href="{{ route('mobile-app-development-dubai') }}">Mobile App Development</a></li>
+            <li><a href="{{ route('performance-marketing-dubai') }}">Performance Marketing</a></li>
+            </ul>
+    </div>
+
+    <!-- SUBSCRIBE -->
+    <div class="footer-col subscribe">
+      <h4>Subscribe US</h4>
+      <p>Make the right business move.</p>
+
+      <form class="subscribe-form">
+        <input type="email" placeholder="Email address">
+        <button type="submit">Send</button>
+      </form>
+
+      <small>Your email ID is confidential.</small>
+
+      <div class="social">
+        <a href="{{ $settingData->twitter_url ?? 'javascript:void(0)' }}"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="{{ $settingData->facebook_url ?? 'javascript:void(0)' }}"><i class="fa-brands fa-facebook-f"></i></a>
+        <a href="{{ $settingData->linkedin_url ?? 'javascript:void(0)' }}"><i class="fa-brands fa-linkedin-in"></i></a>
+        <a href="{{ $settingData->website_url ?? 'javascript:void(0)' }}"><i class="fa-brands fa-youtube"></i></a>
+        <a href="{{ $settingData->instagram_url ?? 'javascript:void(0)' }}"><i class="fa-brands fa-instagram"></i></a>
+      </div>
+    </div>
+
+  </div>
